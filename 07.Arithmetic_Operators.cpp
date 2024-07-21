846aa042a67a68b2afda2cab1eea1530b9ec5990
root <root@DESKTOP-OI4Q969>
2024-07-21 15:34:21 +0300

    Arithmetic_Operators

---
+++ 07.Arithmetic_Operators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    short int a, b;
+    cout << "Enter the first number a: ";
+    cin >> a;
+    cout << "Enter the first number b: ";
+    cin >> b;
+
+    cout << "a + b = " << a + b << endl;
+    cout << "a - b = " << a - b << endl;
+    cout << "a * b = " << a * b << endl;
+    cout << "a / b = " << a / b << endl;
+    cout << "a % b = " << a % b << endl;
+
+}
