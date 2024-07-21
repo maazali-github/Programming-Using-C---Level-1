ba51c4bed0cd4c0ddf81dbe604ffee368cca059f
root <root@DESKTOP-OI4Q969>
2024-07-21 15:33:24 +0300

    user_input

---
+++ 06.user_input.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char MyChar;
+    int MyNumber;
+
+    cout << "Please Enter a character: \n";
+    cin >> MyChar;
+
+    cout << "Please Enter a number: \n";
+    cin >> MyNumber;
+
+    cout << "============\n";
+    cout << MyChar << endl; 
+    cout << MyNumber << endl; 
+
+
+    return 0;
+}
