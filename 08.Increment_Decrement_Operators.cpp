7e664a1a7cfd2dc41225217d5101a27cd6387463
root <root@DESKTOP-OI4Q969>
2024-07-21 15:36:11 +0300

    Increment_Decrement_Operators

---
+++ 08.Increment_Decrement_Operators.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    short a = 10, b = 5;
+    a++;
+    b--;
+
+    // ++a
+    // --b
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    return 0;
+}
