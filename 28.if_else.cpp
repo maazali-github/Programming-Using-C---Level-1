16ba3ebb6a9a5332d4a958cf190010fcbf937388
root <root@DESKTOP-OI4Q969>
2024-07-23 16:31:21 +0300

    if_else

---
+++ 28.if_else.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cout << "Enter a number: ";
+    cin >> x;
+
+    if (x > 5)
+    {
+        cout << "The code of if is executed.\n";
+    }
+    else
+    {
+        cout << "The code of else is executed.\n";
+    }
+
+    return 0;
+}
